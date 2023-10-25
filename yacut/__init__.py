3810b4138f07952ca85381bdf89e49bf9296c84b
Anastasia Sokolova <navukso@ya.ru>
2023-10-25 21:08:49 +0300

    Done

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -15,7 +15,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from yacut import api_views, cli_commands, errors, forms, models, views
+from yacut import api_views, errors, forms, models, views
 
 if __name__ == "__main__":
     app.run()
