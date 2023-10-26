8b52b38d8e7c302bdd7b4fe44ae3a17307db6d5c
Anastasia Sokolova <navukso@ya.ru>
2023-10-26 12:01:36 +0300

    Done

--- settings.py
+++ settings.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("TRACK_MODIFICATIONS")
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('TRACK_MODIFICATIONS')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SUP3R')
 
 
-CHARACTERS = "123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
-SPECIAL_CHARS = "$&!,|.><-; "
-PERMANENT_PART = "http://localhost/"
+CHARACTERS = '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+SPECIAL_CHARS = '$&!,|.><-; '
+PERMANENT_PART = 'http://localhost/'
